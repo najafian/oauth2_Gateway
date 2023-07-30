fc2188e8cc5be0a17360dba3b1982fbe69058535
Mehdi Najafian <najafian@live.com>
2023-07-30 09:23:23 +0200

    init

---
+++ src/main/kotlin/com/helia/oauth/OauthApplication.kt
@@ -0,0 +1,11 @@
+package com.helia.oauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthApplication
+
+fun main(args: Array<String>) {
+	runApplication<OauthApplication>(*args)
+}
